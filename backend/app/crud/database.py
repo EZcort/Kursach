6bab8ee345504660a2eef457726b0268767d9269
EZcort <969demonio696@gmail.com>
2025-09-23 23:37:15 +0300

    Auth

--- backend/app/crud/database.py
+++ backend/app/crud/database.py
@@ -1,35 +1,22 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-import os
-from dotenv import load_dotenv
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:123456@localhost/Web-site')
-engine = create_engine(DATABASE_URL)
-Base = declarative_base()
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class Base(DeclarativeBase):
+    pass
 
 
 class User(Base):
     __tablename__ = 'users'
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
-    full_name = Column(String(255), nullable=False)
-    role = Column(String(20), default='user')
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_name: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[str] = mapped_column(String(20), default='user')
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,30 @@
 from fastapi import FastAPI, HTTPException, Response, Depends
 from authx import AuthX, AuthXConfig
 from app.schemas.users import UserLogicSchema
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import text
+import asyncio
+
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+# engine = create_async_engine(DATABASE_URL)
+# async def get1():
+#     async with engine.connect() as connection:
+#         res = await connection.execute(text('select 1,2,3 union select 4,5,6'))
+#         print(f'{res.first()=}')
+# asyncio.run(get1())
 
 
 app = FastAPI()
+
 
 config = AuthXConfig()
 config.JWT_SECRET_KEY = 'SECRET_KEY'
 config.JWT_ACCESS_COOKIE_NAME = 'my_access_token'
 config.JWT_TOKEN_LOCATION = ['cookies']
-
 security = AuthX(config=config)
 
 

f62d51f6375d4455a46da02b5501d2991438af16
EZcort <969demonio696@gmail.com>
2025-09-30 11:37:02 +0300

    322

---
+++ backend/app/routers/Auth.py
@@ -0,0 +1,29 @@
+from authx import AuthX, AuthXConfig
+from app.schemas.users import UserLogicSchema
+from fastapi import APIRouter, HTTPException, Response, Depends
+
+config = AuthXConfig()
+config.JWT_SECRET_KEY = 'SECRET_KEY'
+config.JWT_ACCESS_COOKIE_NAME = 'my_access_token'
+config.JWT_TOKEN_LOCATION = ['cookies']
+security = AuthX(config=config)
+
+router = APIRouter(prefix='/Authorization', tags=['Authorization'])
+
+
+@router.post('/login-cookie')
+async def login(creds: UserLogicSchema, response: Response):
+    if creds.username == 'test' and creds.password == 'test':
+        token = security.create_access_token(uid='12345')
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        return {'access_token': token}
+    raise HTTPException(status_code=401, detail='Некорректные данные пользователя')
+
+@router.get('/Protected', dependencies=[Depends(security.access_token_required)])
+async def protected():
+    return {'data': 'SECRET'}
+
+
+@router.get('/')
+async def root():
+    return {'message': 'Start!'}
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI, HTTPException, Response, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from authx import AuthX, AuthXConfig
-from app.schemas.users import UserLogicSchema
 import os
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy import text
-import asyncio
+import app.routers.Auth as Auth
+# from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+# from sqlalchemy import text
+# import asyncio
 
 
 load_dotenv()
@@ -14,14 +13,16 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 app = FastAPI()
+routers = [Auth.router]
+[app.include_router(router) for router in routers]
 # Настройка CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # URL вашего фронтенда
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:3000"],  # URL вашего фронтенда
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 # engine = create_async_engine(DATABASE_URL)
 # async def get1():
 #     async with engine.connect() as connection:
@@ -30,26 +31,7 @@ app.add_middleware(
 # asyncio.run(get1())
 
 
-config = AuthXConfig()
-config.JWT_SECRET_KEY = 'SECRET_KEY'
-config.JWT_ACCESS_COOKIE_NAME = 'my_access_token'
-config.JWT_TOKEN_LOCATION = ['cookies']
-security = AuthX(config=config)
 
 
-@app.post('/login')
-async def login(creds: UserLogicSchema, response: Response):
-    if creds.username == 'test' and creds.password == 'test':
-        token = security.create_access_token(uid='12345')
-        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
-        return {'access_token': token}
-    raise HTTPException(status_code=401, detail='Некорректные данные пользователя')
-
-@app.get('/protected', dependencies=[Depends(security.access_token_required)])
-async def protected():
-    return {'data': 'SECRET'}
 
 
-@app.get('/')
-async def root():
-    return {'message': 'Start!'}
